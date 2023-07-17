4da5262591507dac64500e47ce8730f7e8182342
musakazim989 <musakazim989@gmail.com>
2023-07-17 17:42:08 +0600

    table added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import TodoList from "./components/TodoList"
+
 function App() {
   return (
     <>
-      <h1 className="text-red-500">hello</h1>
+      <div className=" border-blue-950 rounded-xl w-3/4 m-auto">
+        <h1 className=" text-blue-700 font-bold text-[72px] text-center mt-6">
+          Todo List
+        </h1>
+        <TodoList />
+      </div>
     </>
   )
 }
